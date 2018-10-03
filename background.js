065ba377ef928a0ab16f6999ed721d8d5800fead
alex <emailforalexgo@gmail.com>
2018-10-02 21:20:00 -0400

    works in chrome and edge

--- background.js
+++ background.js
@@ -1,178 +1,215 @@
-class Utils {
+let client
 
+try {
+  client = browser
+} catch (e) {
+  try {
+      client = chrome
+  } catch (f) {
+      client = null
+  }
+}
+
+class Utils {
   updateBadgeText(text) {
-    chrome.browserAction.setBadgeText({text: '' + text});
-  };
+    client.browserAction.setBadgeText({text: '' + text})
+  }
 
   closeTab(id) {
-    chrome.tabs.remove(id);
-  };
+    client.tabs.remove(id)
+  }
 
   createTab(url) {
-    chrome.tabs.create({url: url});
+    client.tabs.create({url: url})
   }
 
   parseBool(str) {
     if(!str) {
-      return false;
+      return false
     }
 
     if(!isNaN(str)) {
       if(Number(str).valueOf() === 0) {
-        return false;
+        return false
       }
-      return true;
+      return true
     }
 
-    if(typeof str === "string") {
-      if(str === "") {
-        return false;
+    if(typeof str === 'string') {
+      if(str === '') {
+        return false
       }
-      if(str.toLowerCase() === "false") {
-        return false;
+      if(str.toLowerCase() === 'false') {
+        return false
       }
-      return true;
+      return true
     }
-  };
+  }
 
 }
 
 class NoNewTabsExtension {
 
-  constructor () {
-    this._utils = new Utils();
-    this._allowAll = this._utils.parseBool(localStorage.getItem('allowAll') || false);
-    this._allowBlankTabs = this._utils.parseBool(localStorage.getItem('allowBlankTabs') || false);
-    this._allowHistory = this._utils.parseBool(localStorage.getItem('allowHistory') || true);
-    this._history = new TabsHistory();
-    this._tabToBeOpened = -1;
+  constructor (windowId) {
+    this.originalWindowId = windowId
+    this._utils = new Utils()
+    this._allowAll = this._utils.parseBool(localStorage.getItem('allowAll') || false)
+    this._allowBlankTabs = this._utils.parseBool(localStorage.getItem('allowBlankTabs') || false)
+    this._allowHistory = this._utils.parseBool(localStorage.getItem('allowHistory') || true)
+    this._history = new TabsHistory()
+    this._tabToBeOpened = -1
   }
 
   get utils () {
-    return this._utils;
+    return this._utils
   }
   get allowAll () {
-    return this._allowAll;
-  };
+    return this._allowAll
+  }
 
   set allowAll (allow) {
-      this._allowAll = allow;
-      localStorage.setItem('allowAll', allow);
-  };
+      this._allowAll = allow
+      localStorage.setItem('allowAll', allow)
+  }
 
   get allowBlankTabs () {
-    return this._allowBlankTabs;
-  };
+    return this._allowBlankTabs
+  }
 
   set allowBlankTabs (allow) {
-    this._allowBlankTabs = allow;
-    localStorage.setItem('allowBlankTabs', allow);
-  };
+    this._allowBlankTabs = allow
+    localStorage.setItem('allowBlankTabs', allow)
+  }
 
   get allowHistory () {
-    return this._allowHistory;
-  };
+    return this._allowHistory
+  }
 
   set allowHistory (allow) {
-    this._allowHistory = allow;
-    localStorage.setItem('allowHistory', allow);
-  };
+    this._allowHistory = allow
+    localStorage.setItem('allowHistory', allow)
+  }
 
   get history () {
-    return this._history.history;
-  };
+    return this._history.history
+  }
 
   openNewTab (url) {
-    this._tabToBeOpened = url;
-    this._utils.createTab(url);
-  };
+    this._tabToBeOpened = url
+    this._utils.createTab(url)
+  }
 
   clearHistory () {
-    this._history.clear();
-    this._utils.updateBadgeText(this._history.totalCount);
-  };
+    this._history.clear()
+    this._utils.updateBadgeText(this._history.totalCount)
+  }
 
   removeFromHistory (url) {
-    this._history.removeFromHistory(url);
-    this._utils.updateBadgeText(this._history.totalCount);
-  };
+    this._history.removeFromHistory(url)
+    this._utils.updateBadgeText(this._history.totalCount)
+  }
 
   processTab(tab) {
-    if(this._tabToBeOpened == tab.url){
-      this._tabToBeOpened = -1;
-      return;
+    if (this._tabToBeOpened == tab.url){
+      this._tabToBeOpened = -1
+      return
     }
 
-    if (tab.url == 'chrome://newtab/' && this._allowBlankTabs) {
-      return;
+    if (tab.index === 0 && tab.windowId === this.originalWindowId) {
+      return
     }
 
-    if(this._allowHistory) {
-      this._history.addToHistory(tab.url);
+    if (this._allowBlankTabs) {
+      switch(tab.url) {
+        case 'chrome://newtab/':
+        case 'about:blank':
+            return
+      }
     }
-    this._utils.updateBadgeText(this._history.totalCount);
-    this._utils.closeTab(tab.id);
-  };
+
+    if (this._allowHistory) {
+      this._history.addToHistory(tab.url)
+    }
+
+    this._utils.updateBadgeText(this._history.totalCount)
+    this._utils.closeTab(tab.id)
+  }
 }
 
 class TabsHistory {
   constructor() {
-    this._history = [];
-    this._totalCount = 0;
+    this._history = []
+    this._totalCount = 0
     this._incrementCount = (by) => {
-      this._totalCount += by;
-    };
+      this._totalCount += by
+    }
     this._decrementCount = (by) => {
-      this._totalCount -= by;
-    };
-  };
+      this._totalCount -= by
+    }
+  }
 
   get totalCount () {
-    return this._totalCount;
-  };
+    return this._totalCount
+  }
 
   get history () {
-    return this._history;
-  };
+    return this._history
+  }
 
   clear () {
-    this._history = [];
-    this._totalCount = 0;
-  };
+    this._history = []
+    this._totalCount = 0
+  }
 
   addToHistory(url) {
-    var previousCount = this._history[url] || 0;
-    this._history[url] = previousCount + 1;
-    this._incrementCount(1);
-  };
+    const previousCount = this._history[url] || 0
+    this._history[url] = previousCount + 1
+    this._incrementCount(1)
+  }
 
   removeFromHistory(url) {
-    var count = this._history[url] || 0;
-    this._decrementCount(count);
-    delete this._history[url];
-  };
+    const count = this._history[url] || 0
+    this._decrementCount(count)
+    delete this._history[url]
+  }
 }
-
-var _noNewTabsExtension = new NoNewTabsExtension();
-
-chrome.tabs.onCreated.addListener(function(tab) {
-    if(_noNewTabsExtension.allowAll) {
-      return;
-    }
-    if(!tab.url) {
-      chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, updatedTab) {
-        if(tabId == tab.id && changeInfo.url) {
-          _noNewTabsExtension.processTab(updatedTab);
-        }
-      });
-    }  else {
-      _noNewTabsExtension.processTab(tab);
-    }
-});
 
 function menuItemClicked(e) {
-  var url = e.linkUrl || e.srcUrl || e.pageUrl;
-  _noNewTabsExtension.openNewTab(url);
+  var url = e.linkUrl || e.srcUrl || e.pageUrl
+  _noNewTabsExtension.openNewTab(url)
 }
 
-chrome.contextMenus.create({"title": "Open New Tab", "id": "parent", contexts: ['all'], onclick: menuItemClicked});
+function createNewExtension(window) {
+  _noNewTabsExtension = new NoNewTabsExtension(window.id)
+  client.tabs.onCreated.addListener(function(tab) {
+    if(_noNewTabsExtension.allowAll) {
+      return
+    }
+    if(!tab.url) {
+        client.tabs.onUpdated.addListener(function(tabId, changeInfo, updatedTab) {
+        if(tabId == tab.id && changeInfo.url) {
+          _noNewTabsExtension.processTab(updatedTab)
+        }
+      })
+    }  else {
+      _noNewTabsExtension.processTab(tab)
+    }
+  })
+  try {
+    client.contextMenus.create({'title': 'Open New Tab', 'id': 'open-new-tab', contexts: ['all']})
+  } catch (e) {
+    console.log("ERROR CREATING CONTEXT MENU: " + e)
+  }
+  
+  client.contextMenus.onClicked.addListener(function(info, tab) {
+      if (info.menuItemId === 'open-new-tab') {
+          menuItemClicked(info)
+      }
+  })
+}
+
+var _noNewTabsExtension
+
+client
+.windows
+.getCurrent({populate: true}, createNewExtension)
